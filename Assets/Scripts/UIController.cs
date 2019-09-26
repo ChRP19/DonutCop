dcc3de34f26127cfac1bfbd502a3c05e0c7f0974
ChRP <r.tchirkov2010@yandex.ru>
2019-09-27 01:23:58 +0300

    Create UIController script

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Text scoreLabel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    }
+
+    public void OnOpenSettings()
+    {
+        Debug.Log("Open settings");
+    }
+}
