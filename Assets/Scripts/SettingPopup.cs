9ac941de7ccd0a0381665c927498a27441509af7
ChRP <r.tchirkov2010@yandex.ru>
2019-09-27 22:24:47 +0300

    Add pause

--- Assets/Scripts/SettingPopup.cs
+++ Assets/Scripts/SettingPopup.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class SettingPopup : MonoBehaviour
 {
-    
+
     public void Open()
     {
         gameObject.SetActive(true);
+        Time.timeScale = 0.0f;
     }
 
     public void Close()
     {
         gameObject.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
