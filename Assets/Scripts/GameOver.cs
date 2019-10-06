33197d54fb35c9aa00a3ec655226d3326964caa9
ChRP <r.tchirkov2010@yandex.ru>
2019-10-06 18:36:36 +0300

    Create GameOver script

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private GameObject UIController;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Barrier"))
+        {
+            UIController.GetComponent<UIController>().RestartGame();
+        }
+    }
+
+}
