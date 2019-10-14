80b7d1490813a3fa5220ae5e63e717e0e979b4d4
Roman <r.tchirkov2010@yandex.ru>
2019-10-14 23:17:06 +0300

    Update ScoreScript.cs

--- Assets/OlderGames/Scripts/ScoreScript.cs
+++ Assets/OlderGames/Scripts/ScoreScript.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Collections;
 
 public class ScoreScript : MonoBehaviour
 {
     [SerializeField] private Text scoreLabel;
     public static float score = 0;
-    public float scoreTime = 0;
+    private float scoreTime = 0;
 
+    void Start()
+    {
+        StartCoroutine(SpeedUp());
+    }
     void FixedUpdate()
     {
         scoreTime = Time.timeSinceLevelLoad;
-        score = (float)Math.Round(scoreTime, 1) * 10;                           //Увеличение счетчика на 1
+        score = (float)Math.Round(scoreTime, 1) * 10;                           
         scoreLabel.text = score.ToString();
+    }
+
+    IEnumerator SpeedUp()
+    {
+        yield return new WaitForSeconds(1);
+        Runner2D.speed = Runner2D.speed + 0.1f;
+        StartCoroutine(SpeedUp());
     }
 }
