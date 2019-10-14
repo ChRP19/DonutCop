e3e35b1a45af1b2917a1e119559b62aca4bcd147
Roman <r.tchirkov2010@yandex.ru>
2019-10-14 23:15:40 +0300

    Create coin script

---
+++ Assets/OlderGames/Scripts/CoinScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinScore : MonoBehaviour
+{
+    [SerializeField]private Text coinLabel;
+    private int coin = 0;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            coin++;
+            coinLabel.text = coin.ToString();
+        }    
+    }
+}
