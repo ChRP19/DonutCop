60d4bb84abfb24586d9022df467312417c063918
Roman <r.tchirkov2010@yandex.ru>
2019-10-20 17:45:11 +0300

    Update ScoreScript.cs

--- Assets/OlderGames/Scripts/ScoreScript.cs
+++ Assets/OlderGames/Scripts/ScoreScript.cs
@@ -9,21 +9,10 @@ public class ScoreScript : MonoBehaviour
     public static float score = 0;
     private float scoreTime = 0;
 
-    void Start()
-    {
-        StartCoroutine(SpeedUp());
-    }
     void FixedUpdate()
     {
         scoreTime = Time.timeSinceLevelLoad;
         score = (float)Math.Round(scoreTime, 1) * 10;                           
         scoreLabel.text = score.ToString();
-    }
-
-    IEnumerator SpeedUp()
-    {
-        yield return new WaitForSeconds(1);
-        Runner2D.speed = Runner2D.speed + 0.1f;
-        StartCoroutine(SpeedUp());
     }
 }
