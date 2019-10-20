7c2188b3afd4b453e0b63a56a075f962ec991ece
Roman <r.tchirkov2010@yandex.ru>
2019-10-20 17:45:01 +0300

    Create coin activator script

---
+++ Assets/OlderGames/Scripts/CoinActivator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject Coin;
+    void Update() 
+    {
+        if (Coin.activeSelf == false)
+        {
+            StartCoroutine(Active());
+        }
+    }
+
+    IEnumerator Active()
+    {
+        yield return new WaitForSeconds(2);
+        //this.gameObject.SetActive(true);
+        Coin.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
