9bf7080822f46560a8f478a6ec9014fbef85d26e
Roman <r.tchirkov2010@yandex.ru>
2019-10-20 17:44:42 +0300

    Create Level speed script

---
+++ Assets/OlderGames/Scripts/LevelSpeed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpeedUp());
+    }
+
+    IEnumerator SpeedUp()
+    {
+        yield return new WaitForSeconds(3);
+        //Runner2D.speed = Runner2D.speed + 0.1f;
+        Time.timeScale = Time.timeScale + 0.01f;
+        StartCoroutine(SpeedUp());
+    }
+}
