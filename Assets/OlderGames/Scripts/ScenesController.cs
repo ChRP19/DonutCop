8d2d249cb396ec6ccaef4f1b6cace03854d3278f
Roman <r.tchirkov2010@yandex.ru>
2019-10-23 20:12:10 +0300

    Assigned coin count

--- Assets/OlderGames/Scripts/ScenesController.cs
+++ Assets/OlderGames/Scripts/ScenesController.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScenesController : MonoBehaviour
 {
+    [SerializeField]private Text coinLabel;
+    void Awake() 
+    {
+        coinLabel.text = (PlayerPrefs.GetInt("coin").ToString());
+    }
     public void PressToStart()
     {
         SceneManager.LoadScene("Game");
