ab325a2cec586a86fa79ccaf72839e541306d493
Roman <r.tchirkov2010@yandex.ru>
2019-10-23 20:10:16 +0300

    Update BGScaler.cs

--- Assets/OlderGames/Scripts/BGScaler.cs
+++ Assets/OlderGames/Scripts/BGScaler.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class BGScaler : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         float height = Camera.main.orthographicSize * 2f;
         float width = height * Screen.width / Screen.height;
 
          transform.localScale = new Vector3(width, height, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
